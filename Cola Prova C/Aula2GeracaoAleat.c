74df5e78f9c11acf2c2e7dfac878afadc2ea5014
Bernardo Gutman Siqueira <102383550+Bernardo-Gutman-Siqueira@users.noreply.github.com>
2023-09-26 20:59:32 -0300

    Update Aula2GeracaoAleat.c

--- Cola Prova C/Aula2GeracaoAleat.c
+++ Cola Prova C/Aula2GeracaoAleat.c
@@ -1,17 +1,15 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h> //cabecalho para uso de funcoes de tempo
+#include<stdio.h> // Inclui o cabeçalho da biblioteca padrão de entrada e saída
+#include<stdlib.h> // Inclui o cabeçalho da biblioteca padrão de funções gerais
+#include<time.h> // Inclui o cabeçalho da biblioteca de funções de tempo
 
-int main(){//metodo principal
-
-	srand(time(NULL)); //corrige o problema da geracao aleatoria repetida
-
-	int i, n, aleatorio; //declaracao de variaveis inteiras
-    
+int main(){ // Função principal do programa
+	srand(time(NULL)); //Corrige o problema da geracao aleatoria repetida
+	int i, n, aleatorio; // Declaração de variáveis inteiras
 	printf("\nQuantos numeros aleatorios deseja? ");
-	scanf("%d", &n); //lendo uma variavel inteira e colocando na memoria de n
-	for(i=0; i<n; i++){
-		aleatorio = rand()%101; //gerando um aleatorio entre 0 e 100
-		printf("\t%4d\n", aleatorio); 
+	scanf("%d", &n); // Lê um número inteiro do usuário e armazena na variável n
+	for(i=0; i<n; i++){ // Loop que executa n vezes
+		aleatorio = rand()%101; // Gera um número aleatório entre 0 e 100
+		printf("\t%4d\n", aleatorio); // Imprime o número aleatório gerado
 	}
+	return 0; // Retorna 0 indicando que o programa terminou com sucesso
 }
