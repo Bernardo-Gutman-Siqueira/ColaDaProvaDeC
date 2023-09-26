b4018c99a93e0328fc4b916041e60df5e1ca7c5b
Bernardo Gutman Siqueira <102383550+Bernardo-Gutman-Siqueira@users.noreply.github.com>
2023-09-26 20:52:14 -0300

    Update Aula2GeracaoAleatDefinida.c

--- Cola Prova C/Aula2GeracaoAleatDefinida.c
+++ Cola Prova C/Aula2GeracaoAleatDefinida.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h> //cabecalho de funcoes de tempo
+#include<stdio.h> // Inclui o cabeçalho da biblioteca padrão de entrada e saída
+#include<stdlib.h> // Inclui o cabeçalho da biblioteca padrão de funções gerais
+#include<time.h> // Inclui o cabeçalho da biblioteca de funções de tempo
 
-int main(){ //metodo principal
-	srand(time(NULL)); //corrigindo o problema de geracao igual de numeros
-	int i, n, inferior, superior; //declarando variaveis inteiras
+int main(){ // Função principal do programa
+	srand(time(NULL)); //Corrigindo o problema de geracao igual de numeros
+	int i, n, inferior, superior; // Declaração de variáveis inteiras
 	printf("\nQuantos numeros aleatorios deseja? ");
-	scanf("%d", &n); //lendo um inteiro e colocando na posicao de memoria de n
-	printf("\nQual o menor número? ");
-	scanf("%d", &inferior);
-	printf("\nQual o maior número? ");
-	scanf("%d", &superior);
+	scanf("%d", &n); // Lê um número inteiro do usuário e armazena na variável n
+	printf("\nQual o menor numero? ");
+	scanf("%d", &inferior); // Lê um número inteiro do usuário e armazena na variável inferior
+	printf("\nQual o maior numero? ");
+	scanf("%d", &superior); // Lê um número inteiro do usuário e armazena na variável superior
 	
-	for(i=0; i<n; i++){ //laco para executando n vezes
-		printf("\t%4d\n", (inferior)+(rand()%(superior-inferior+1))); //uma tabulacao depois 4 espacos
-	}
+	for(i=0; i<n; i++){ // Loop que executa n vezes
+		printf("\t%4d\n", (inferior)+(rand()%(superior-inferior+1))); // Imprime um número aleatório entre inferior e superior (inclusive)
+	} //  /t = uma tabulacao depois 4 espacos
+	return 0; // Retorna 0 indicando que o programa terminou com sucesso
 }
