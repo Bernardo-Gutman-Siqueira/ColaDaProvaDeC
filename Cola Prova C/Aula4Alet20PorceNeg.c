53b71a937869c935e77ec7ffe757e2f48121f9c6
Bernardo Gutman Siqueira <102383550+Bernardo-Gutman-Siqueira@users.noreply.github.com>
2023-09-26 21:20:02 -0300

    Update Aula4Alet20PorceNeg.c

--- Cola Prova C/Aula4Alet20PorceNeg.c
+++ Cola Prova C/Aula4Alet20PorceNeg.c
@@ -1,21 +1,23 @@
 // 2) Faça um programa que gere números aleatórios reais
 // entre -100 e 100 com 20% de chance de ser negativo.
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
+#include<stdio.h> // Inclui o cabeçalho da biblioteca padrão de entrada e saída
+#include<stdlib.h> // Inclui o cabeçalho da biblioteca padrão de funções gerais
+#include<time.h> // Inclui o cabeçalho da biblioteca de funções de tempo
 //gerar n numeros reais entre -100 e 100 com 20% de negativos
-main(){
-	srand(time(NULL)); //setar o gerador de aleatorios
-	int i, n;
+// Função principal do programa
+int main(){
+	srand(time(NULL)); // Inicializa a semente do gerador de números aleatórios com o tempo atual
+	int i, n; // Declaração de variáveis inteiras
 	printf("\nQuantos numeros reais? ");
-	scanf("%d", &n);
-	float aleatorio;
-	for(i=0; i<n; i++){
-		aleatorio = ((float)rand()/(float)RAND_MAX)*100.0; //gera um numero entre 0 e 100
-		if(rand()%100 < 20){ // 20 em 100 numeros (20%)
-			aleatorio*=-1; //transformo o numero em negativo
+	scanf("%d", &n); // Lê um número inteiro do usuário e armazena na variável n
+	float aleatorio; // Declaração de variável de ponto flutuante
+	for(i=0; i<n; i++){ // Loop que executa n vezes
+		aleatorio = ((float)rand()/(float)RAND_MAX)*100.0; // Gera um número aleatório entre 0 e 100
+		if(rand()%100 < 20){ // Se um número aleatório entre 0 e 99 é menor que 20 (20% de chance)
+			aleatorio*=-1; // Transforma o número em negativo
 		}
-		printf("%15.10f\n", aleatorio);
+		printf("%15.10f\n", aleatorio); // Imprime o número aleatório gerado
 	}
+	return 0; // Retorna 0 indicando que o programa terminou com sucesso
 }
