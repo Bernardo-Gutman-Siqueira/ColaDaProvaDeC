04b666577168e17fbaeab263509483ebc6327ac4
Bernardo Gutman Siqueira <102383550+Bernardo-Gutman-Siqueira@users.noreply.github.com>
2023-09-26 21:31:56 -0300

    Update Aula4AletPlacasMerc.c

--- Cola Prova C/Aula4AletPlacasMerc.c
+++ Cola Prova C/Aula4AletPlacasMerc.c
@@ -1,23 +1,23 @@
 // 5) Faça um programa que gere n, aleatórias, placas de veículos no padrão mercosul.
 
-#include<stdio.h>
-#include<time.h>
-#include<math.h>
-#include<stdlib.h>
+#include<stdio.h> // Inclui o cabeçalho da biblioteca padrão de entrada e saída
+#include<time.h> // Inclui o cabeçalho da biblioteca de funções de tempo
+#include<stdlib.h> // Inclui o cabeçalho da biblioteca padrão de funções gerais
 //gerando placas mercosul aleatorias
-main(){
-	srand(time(NULL));
-	int i, n;
-	float aleat;
+// Função principal do programa
+int main(){
+	srand(time(NULL)); // Inicializa a semente do gerador de números aleatórios com o tempo atual
+	int i, n; // Declaração de variáveis inteiras
 	printf("\nQuantas placas deseja? ");
-	scanf("%d", &n);
-	for(i=0; i<n; i++){
-		printf("%c", 65+rand()%23);
-		printf("%c", 65+rand()%23);
-		printf("%c - ", 65+rand()%23);
-		printf("%d", rand()%10);
-		printf("%c", 65+rand()%23);
-		printf("%d", rand()%10);
-		printf("%d\n", rand()%10);
+	scanf("%d", &n); // Lê um número inteiro do usuário e armazena na variável n
+	for(i=0; i<n; i++){ // Loop que executa n vezes
+		printf("%c", 65+rand()%23); // Imprime um caractere aleatório entre 'A' e 'W'
+		printf("%c", 65+rand()%23); // Imprime um caractere aleatório entre 'A' e 'W'
+		printf("%c - ", 65+rand()%23); // Imprime um caractere aleatório entre 'A' e 'W'
+		printf("%d", rand()%10); // Imprime um dígito aleatório entre 0 e 9
+		printf("%c", 65+rand()%23); // Imprime um caractere aleatório entre 'A' e 'W'
+		printf("%d", rand()%10); // Imprime um dígito aleatório entre 0 e 9
+		printf("%d\n", rand()%10); // Imprime um dígito aleatório entre 0 e 9
 	}
+	return 0; // Retorna 0 indicando que o programa terminou com sucesso
 }
