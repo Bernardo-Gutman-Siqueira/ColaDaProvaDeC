b51012274daaf286e6e4ba8dafd6a6870db24f8e
Bernardo Gutman Siqueira <102383550+Bernardo-Gutman-Siqueira@users.noreply.github.com>
2023-09-26 21:34:49 -0300

    Update Aula4AletReal.c

--- Cola Prova C/Aula4AletReal.c
+++ Cola Prova C/Aula4AletReal.c
@@ -1,18 +1,19 @@
 // 1) Faça um programa que gere números aleatórios reais.
 
-#include<stdio.h>
-#include<time.h>
-#include<math.h>
-#include<stdlib.h>
-//gerando aleatorios reais
-main(){
-	srand(time(NULL));
-	int i, n;
-	float aleat;
+#include<stdio.h> // Inclui o cabeçalho da biblioteca padrão de entrada e saída
+#include<time.h> // Inclui o cabeçalho da biblioteca de funções de tempo
+#include<stdlib.h> // Inclui o cabeçalho da biblioteca padrão de funções gerais
+// Gerando aleatorios reais
+// Função principal do programa
+int main() {
+	srand(time(NULL)); // Inicializa a semente do gerador de números aleatórios com o tempo atual
+	int i, n; // Declaração de variáveis inteiras
+	float aleat; // Declaração de variável de ponto flutuante
 	printf("\nQuantos numeros reais deseja? ");
-	scanf("%d", &n);
-	for(i=0; i<n; i++){
-		aleat = ((float)rand()/(float)RAND_MAX); //gera um numero aleatorio entre 0 e 1
-		printf("%5d numero gerado\t%.10f\n", (i+1), aleat);
+	scanf("%d", &n); // Lê um número inteiro do usuário e armazena na variável n
+	for(i=0; i<n; i++){ // Loop que executa n vezes
+		aleat = ((float)rand()/(float)RAND_MAX); // Gera um número aleatório entre 0 e 1
+		printf("%5d numero gerado\t%.10f\n", (i+1), aleat); // Imprime o número aleatório gerado
 	}
+	return 0; // Retorna 0 indicando que o programa terminou com sucesso
 }
