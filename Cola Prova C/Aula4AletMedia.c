f17f215107dce65ca8f9bfceff4269d9d28483d4
Bernardo Gutman Siqueira <102383550+Bernardo-Gutman-Siqueira@users.noreply.github.com>
2023-09-26 21:28:57 -0300

    Update Aula4AletMedia.c

--- Cola Prova C/Aula4AletMedia.c
+++ Cola Prova C/Aula4AletMedia.c
@@ -2,41 +2,43 @@
 // entre A e B (dados passados pelo usuário), 
 // após isso diga o maior e o menor números gerados, 
 
-#include<stdio.h>
-#include<time.h>
-#include<math.h>
-#include<stdlib.h>
+#include<stdio.h> // Inclui o cabeçalho da biblioteca padrão de entrada e saída
+#include<time.h> // Inclui o cabeçalho da biblioteca de funções de tempo
+#include<stdlib.h> // Inclui o cabeçalho da biblioteca padrão de funções gerais
+
+// Função principal do programa
 //gerando aleatorios reais entre A e B com calculo da media
-main(){
-	srand(time(NULL));
-	float A, B, media, soma=0, menor, maior, aux, aleat;
-	int i, n;
+int main(){
+	srand(time(NULL)); // Inicializa a semente do gerador de números aleatórios com o tempo atual
+	float A, B, media, soma=0, menor, maior, aux, aleat; // Declaração de variáveis de ponto flutuante
+	int i, n; // Declaração de variáveis inteiras
 	printf("\nQual o valor inferior? ");
-	scanf("%f", &A);
+	scanf("%f", &A); // Lê um número real do usuário e armazena na variável A
 	printf("\nQual o valor superior? ");
-	scanf("%f", &B);
-	if(A > B){ //o usuario digitou invertido, entao vamos trocar
+	scanf("%f", &B); // Lê um número real do usuário e armazena na variável B
+	if(A > B){ // Se o usuário digitou os valores invertidos, então troca os valores de A e B
 		aux = A;
 		A = B;
 		B = aux;
 	}
-	maior = A; //maior recebe o menor valor que pode ser gerado
-	menor = B; //menor recebe o maior valor que pode ser gerado
+	maior = A; // O maior recebe o menor valor que pode ser gerado
+	menor = B; // O menor recebe o maior valor que pode ser gerado
 	printf("\nQuantos numeros deseja? ");
-	scanf("%d", &n);
-	for(i=0; i<n; i++){
-		aleat = ((float)rand()/(float)RAND_MAX)*(B-A)+A;
-		soma+=aleat; //somando os valores gerados
+	scanf("%d", &n); // Lê um número inteiro do usuário e armazena na variável n
+	for(i=0; i<n; i++){ // Loop que executa n vezes
+		aleat = ((float)rand()/(float)RAND_MAX)*(B-A)+A; // Gera um número aleatório entre A e B
+		soma+=aleat; // Soma os valores gerados
 		if(aleat > maior){
-			maior = aleat; //o valor é maior que o anterior
+			maior = aleat; // O valor é maior que o anterior
 		}
 		if(aleat < menor){
-			menor = aleat; // o valor é menor que o anterior
+			menor = aleat; // O valor é menor que o anterior
 		}
-		printf("\n%20.5f", aleat);
+		printf("\n%20.5f", aleat); // Imprime o número aleatório gerado
 	}
-	media = soma / (float)n;
-	printf("\n\nO maior valor gerado foi %.10f\n", maior);
-	printf("\n\nO menor valor gerado foi %.10f\n", menor);
-	printf("\nA media de numeros gerados foi %.10f\n\n", media);
+	media = soma / (float)n; // Calcula a média dos números gerados
+	printf("\n\nO maior valor gerado foi %.10f\n", maior); // Imprime o maior valor gerado
+	printf("\n\nO menor valor gerado foi %.10f\n", menor); // Imprime o menor valor gerado
+	printf("\nA media de numeros gerados foi %.10f\n\n", media); // Imprime a média dos números gerados
+	return 0; // Retorna 0 indicando que o programa terminou com sucesso
 }
