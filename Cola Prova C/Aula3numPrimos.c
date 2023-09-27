c80ee4e8dac9a6686c1308a05479ed961f3f24e0
Bernardo Gutman Siqueira <102383550+Bernardo-Gutman-Siqueira@users.noreply.github.com>
2023-09-26 21:17:45 -0300

    Update Aula3numPrimos.c

--- Cola Prova C/Aula3numPrimos.c
+++ Cola Prova C/Aula3numPrimos.c
@@ -1,24 +1,26 @@
 // 3) Faça um programa para apresentar os n primeiros números primos.
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<math.h>
+#include<stdio.h> // Inclui o cabeçalho da biblioteca padrão de entrada e saída
+#include<stdlib.h> // Inclui o cabeçalho da biblioteca padrão de funções gerais
+#include<math.h> // Inclui o cabeçalho da biblioteca de funções matemáticas
 //Numeros Primos
+// Função principal do programa
 int main(){
-	int i, j, ehPrimo, qtd;
+	int i, j, ehPrimo, qtd; // Declaração de variáveis inteiras
 	printf("\nQuantos numeros primos? ");
-	scanf("%d", &qtd);
-	for(i=1; i<=qtd; i++){
-		ehPrimo = 1; //considero que todo numero eh primo ate prova contraria
-		for(j=sqrt(i); j>1; j--){
-			if(i%j == 0){ //se o resto da divisao for 0
-				ehPrimo = 0; // nao eh primo
+	scanf("%d", &qtd); // Lê um número inteiro do usuário e armazena na variável qtd
+	for(i=1; i<=qtd; i++){ // Loop que executa qtd vezes
+		ehPrimo = 1; // Considero que todo numero eh primo ate prova contraria
+		for(j=sqrt(i); j>1; j--){ // Loop que executa da raiz quadrada de i até 2
+			if(i%j == 0){ // Se o resto da divisão de i por j for 0
+				ehPrimo = 0; // O número não é primo
 			}
 		}
-		if(ehPrimo == 1){
-			printf("%d eh Primo\n", i);
+		if(ehPrimo == 1){ // Se o número é primo
+			printf("%d eh Primo\n", i); // Imprime que o número é primo
 		}else{
-			qtd++; //testo mais um numero
+			qtd++; // Testa mais um número
 		}
 	}
+	return 0; // Retorna 0 indicando que o programa terminou com sucesso
 }
